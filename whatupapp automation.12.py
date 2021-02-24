e2b61fffd12c000d2c77d39c06a06d598ce0f9bb
Manjusha_patil_21 <60638877+Manjusha1298@users.noreply.github.com>
2021-02-24 11:11:45 +0530

    Add files via upload

---
+++ whatupapp automation.12.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pyautogui
+
+
+# In[2]:
+
+
+import webbrowser as wb
+
+
+# In[3]:
+
+
+import time
+
+
+# In[4]:
+
+
+wb.open("web.whatsapp.com")
+
+
+# In[5]:
+
+
+time.sleep(30)
+
+
+# In[7]:
+
+
+import pywhatkit
+
+
+# In[ ]:
+
+
+pywhatkit.sendwhatmsg("+91","Hello",15,00)
+
+
+# In[ ]:
+
+
+
+
